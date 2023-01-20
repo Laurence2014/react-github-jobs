edef6208a7ecfe6ac91a651b3c5358cd629e4684
Laurence2014 <kidulaura2014@yahoo.fr>
2023-01-20 17:15:44 +0200

    first trial

---
+++ github-jobs/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page)
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+  )
+}
+
+export default App;
